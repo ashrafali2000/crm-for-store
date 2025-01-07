c914ed0a4397f8f0a93bbe9685e4f6f47ff7f24c
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-07 14:29:47 +0500

    Delete backend/configs directory

--- backend/configs/db.js
+++
@@ -1,7 +0,0 @@
-//This is Configuration file used for connect to database to server by mongoose;
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connection = mongoose.connect(process.env.dbURL);
-
-module.exports = { connection };
